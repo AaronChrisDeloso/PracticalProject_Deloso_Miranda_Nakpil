62116d2315d040ce1f3bce8aef4c50cee538851c
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2022-06-09 04:27:08 +0800

    adding carousel as landing page

--- PracticalProject/PracticalProject/App.xaml.cs
+++ PracticalProject/PracticalProject/App.xaml.cs
@@ -10,7 +10,7 @@ namespace PracticalProject
         {
             InitializeComponent();
 
-            MainPage = new NavigationPage(new MainPage());
+            MainPage = new NavigationPage(new View.LandingPage());
         }
 
         protected override void OnStart()
--- PracticalProject/PracticalProject/View/LandingPage.xaml.cs
+++ PracticalProject/PracticalProject/View/LandingPage.xaml.cs
@@ -1,18 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+
 using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
 
-[assembly: ExportFont("exposure.co", Alias = "Adobe Garamond")]
-
-namespace PracticalProject
+namespace PracticalProject.View
 {
-    public partial class MainPage : ContentPage
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class LandingPage : CarouselPage
     {
-        public MainPage()
+        public LandingPage()
         {
             InitializeComponent();
         }
