fa8ca8abefb71e159008dddb8e80138606e84e8a
Aaron Chris Deloso <deloso.aaronchris@auf.edu.ph>
2022-06-08 22:56:26 +0800

    Add project files.

---
+++ PracticalProject/PracticalProject/ViewModel/AnimalViewModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Microsoft.EntityFrameworkCore;
+using PracticalProject.Model;
+using System.Threading.Tasks;
+
+namespace PracticalProject.ViewModel
+{
+    internal class AnimalViewModel
+    {
+        private Services.DatabaseContext getContext()
+        {
+            return new Services.DatabaseContext();
+        }
+        public int InsertAnimal(AnimalModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Animals.Add(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+        public async Task<List<AnimalModel>> GetAllAnimals()
+        {
+            var _dbContext = getContext();
+            var res = await _dbContext.Animals.ToListAsync();
+            return res;
+        }
+        public int DeleteAnimal(AnimalModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Animals.Remove(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+
+        public async Task<int> UpdateAnimal(AnimalModel obj)
+        {
+            var _dbContext = getContext();
+            _dbContext.Animals.Update(obj);
+            int c = _dbContext.SaveChanges();
+            return c;
+        }
+    }
+}
